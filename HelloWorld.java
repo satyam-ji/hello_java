ecf712a592abf619e931ee5f023a85b6e5539761
Satya <alwaysbesmartguru@gmail.com.(none)>
2022-06-04 16:15:40 +0000

    name is chanhed  this

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {   
       public static void main(String[] args)
       {       
-         System.out.println("Hello satyam-garu - doapati");   
+         System.out.println("Hello satyanarayana doapati");   
       }
 } 
